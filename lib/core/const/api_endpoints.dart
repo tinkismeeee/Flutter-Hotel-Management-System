896b52ecc3626df6fa7ea143220d3e367f16fbbd
tinkismeeee <tinhisme1@gmail.com>
2026-07-02 05:05:34 +0700

    Add unsplash API to fetch random room photos

--- lib/core/const/api_endpoints.dart
+++ lib/core/const/api_endpoints.dart
@@ -1,4 +1,6 @@
 class ApiEndpoints {
+  static const String unsplashBaseUrl = "https://api.unsplash.com"; //https://api.unsplash.com/search/photos/?client_id=3uOtwyvGgTxcSq5xkqOfIf-JwJqqpJQD7BO-HV7_DDE&query=room
+  static const String clientId = "3uOtwyvGgTxcSq5xkqOfIf-JwJqqpJQD7BO-HV7_DDE";
   static const String baseUrl = "http://143.198.221.127/api";
   static const String customer = "$baseUrl/customer";
   static const String room = "$baseUrl/rooms";
