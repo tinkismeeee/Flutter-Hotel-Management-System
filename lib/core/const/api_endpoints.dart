a8a94de536cf9617072f52c53520e9b630792355
tinkismeeee <tinhisme1@gmail.com>
2026-07-03 15:45:34 +0700

    Add booking, invoice, review, report, promotion endpoints

--- lib/core/const/api_endpoints.dart
+++ lib/core/const/api_endpoints.dart
@@ -9,6 +9,11 @@ class ApiEndpoints {
   static const String room = "$baseUrl/rooms";
   static const String roomTypes = "$baseUrl/room-types";
   static const String service = "$baseUrl/services";
+  static const String booking = "$baseUrl/bookings";
+  static const String invoice = "$baseUrl/invoices";
+  static const String review = "$baseUrl/reviews";
+  static const String report = "$baseUrl/reports";
+  static const String promotion = "$baseUrl/promotions";
   static String customerByEmail(String email) => "$customer/email/$email";
   static String customerById(String id) => "$customer/$id";
 }
